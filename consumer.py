77053b580beb6b1a64c904a45d68cb9ccf51aa11
mihai337 <constantinm7787@gmail.com>
2023-08-06 14:36:55 -0500

    tranzactii separate

--- consumer.py
+++ consumer.py
@@ -3,6 +3,7 @@ from database import Database
 from json import loads,dumps
 import requests
 from models import User
+from pprint import pprint
 
 log = False
 NO_OF_TRIES = 3
@@ -17,6 +18,30 @@ def process(message):
         Database.coll.update_one({"name" : message["name"]} , {"$set" : {"balance" : balance}})
         return {"Update" : "Successful"}
     return {"Error" : "Not found"}
+
+def transaction():
+
+    choice = input("\nDo you want to complete all transactions?y/n\n")
+
+    if choice == "y":
+        payload={
+            "name" : username,
+            "password" : password
+        }
+        payload = dumps(payload)
+        requests.post("http://localhost:8000/transactions" , data=payload)
+
+        while log:
+            message_json = Rds.redis_conn.brpop("mod_queue")
+            x,y = message_json
+            message = loads(y)
+            if message["name"] == username:
+                process(message)
+            else:
+                Rds.redis_conn.lpush("mod_queue" , dumps(message))
+    else:
+        print("\nNe mai auzim")
+        exit()
 
 def login():
 
@@ -34,14 +59,15 @@ def login():
 
         response = requests.post("http://localhost:8000/login" , data=payload)
         if response.status_code == 200:
+            results = Database.coll.find({"name" : username})
+            for result in results:
+                pprint(result)
             print("\nLogin successfull\n")
             return True
     else:
-        return False
-            
+        return False           
 
 def sign_in():
-
     for i in range(NO_OF_TRIES):
         username = input("Username : ")
         password = input("Password : ")
@@ -61,7 +87,6 @@ def sign_in():
             print("\nTry again later\n")
             exit()
 
-
 choice = int(input("choices :\n  1. Sign-in\n  2. Login\n"))
 
 if choice == 1:
@@ -72,11 +97,4 @@ else:
     print("choice not valid")
     exit()
 
-while log:
-    message_json = Rds.redis_conn.brpop("mod_queue")
-    x,y = message_json
-    message = loads(y)
-    if message["name"] == username:
-        process(message)
-    else:
-        Rds.redis_conn.lpush("mod_queue" , dumps(message))
+transaction()
--- main.py
+++ main.py
@@ -9,17 +9,6 @@ from json import dumps
 app = FastAPI()
 
 
-def send_transactions(name : str , nr_msg : int):
-    for i in range(nr_msg):
-        value = random.randint(-20,20)
-        message = {
-            "name" : name,
-            "value" : value
-        }
-        m = dumps(message)
-        Rds.redis_conn.lpush("mod_queue" , m)
-
-
 @app.get("/")
 def home():
     raise HTTPException(status_code=status.HTTP_200_OK)
@@ -39,9 +28,19 @@ def login(user : User):
     results = Database.coll.find({"name" : user.name})
     for result in results:
         if result["password"] == user.password:
-            send_transactions(user.name,5)
             raise HTTPException(status_code=status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+@app.post("/transactions")
+def send(user : User):
+    for i in range(5):
+        value = random.randint(-20,20)
+        message = {
+            "name" : user.name,
+            "value" : value
+        }
+        mjson = dumps(message)
+        Rds.redis_conn.lpush("mod_queue" , mjson)
 
 
 print(Database.mongo_db.list_database_names())
