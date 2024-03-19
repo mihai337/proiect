a071fe90c7fe461c1591a68401e2aac172d727cd
mihai337 <constantinm7787@gmail.com>
2024-03-19 18:06:21 +0200

    gg mage atat zic

--- mod.py
+++ mod.py
@@ -1,9 +1,9 @@
 from database import Database
 
-# Database.coll.update_one({"name" : "Mihai"} , {"$set" : {"balance" : 420}})
+Database.coll.update_one({"name" : "admin"} , {"$set" : {"type" : "fact"}})
 
 # Database.coll.delete_one({"name" : "Mihai"})
 # Database.coll.delete_one({"name" : "Dora"})
 # Database.coll.delete_one({"name" : "Mage"})
-Database.coll.delete_one({"name" : "Admin"})
+# Database.coll.delete_one({"name" : "Admin"})
 #just a comment
--- msg_broker.py
+++ msg_broker.py
@@ -2,5 +2,6 @@ from redis import Redis
 from rq import Queue
 
 class Rds:
-    redis_conn = Redis(host="localhost" , port=6379)
-    queue = Queue("mod_queue" , connection=redis_conn)
+    def __init__(self,name) -> None:
+        self.redis_conn = Redis(host="localhost" , port=6379)
+        self.queue = Queue(name , connection=self.redis_conn)
