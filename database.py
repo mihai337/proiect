e5eb42ae9e9e0c8eba1da2994736052b5347f32e
mihai337 <constantinm7787@gmail.com>
2024-05-25 21:55:18 +0300

    final

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 class Database:
-    mongo_db = MongoClient("mongodb://127.0.0.1:81")
+    mongo_db = MongoClient("mongodb://127.0.0.1:27017")
     db = mongo_db["User"]
     coll = db["Data"]
     history = db["History"]
