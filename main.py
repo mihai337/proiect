5682cc371cf49857350b6ca1a9442cc976a315fb
Mihai Constantin <constantinm7787@gmail.com>
2025-01-12 22:37:21 +0200

    create fix

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def whoami(user : dict = Depends(verify_token)):
 @app.post("/create")
 def create_user(createUserRequest : CreateUserRequest, user : dict = Depends(verify_token)):
 
-    if "fact" not in createUserRequest.type or "user" not in createUserRequest.type:
+    if createUserRequest.type not in ["fact", "user"]:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid type")
 
     doc_ref = firestore_db.collection("users").document(user['uid'])
